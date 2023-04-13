f8a176a4aa7e0dbbaa9268025704128e2fec12cc
Groune <98833627+gregoirevaillant@users.noreply.github.com>
2023-04-13 22:00:59 +0200

    Finihs outline

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward, FaUser, FaLaptopCode } from "react-icons/fa";
 
 const About = () => {
     return (
@@ -9,15 +11,40 @@ const About = () => {
 
             <div className="container about__container">
                 <div className="about__me">
-                    <div className="about__me-image"></div>
+                    <div className="about__me-image">
+                        <img src={ME} alt="About me" />
+                    </div>
                 </div>
                 <div className="about__content">
                     <div className="about__cards">
                         <article className="about__card">
+                            <FaAward className="about__icon" />
                             <h5>Experience</h5>
                             <small>3+ Years Working</small>
                         </article>
+                        <article className="about__card">
+                            <FaUser className="about__icon" />
+                            <h5>Clients</h5>
+                            <small>200+ Worldwide</small>
+                        </article>
+                        <article className="about__card">
+                            <FaLaptopCode className="about__icon" />
+                            <h5>Projects</h5>
+                            <small>80+ Completed</small>
+                        </article>
                     </div>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Saepe sunt neque numquam sed sint suscipit ad
+                        doloribus quibusdam magni odit, fuga tenetur hic vero
+                        mollitia adipisci distinctio provident nulla molestiae.
+                        Eveniet asperiores similique molestiae recusandae atque?
+                        Aliquid fugiat illo, voluptatem, ex aliquam deserunt.
+                    </p>
+                    <a href="#contact" className="btn btn-primary">
+                        Let's Talk
+                    </a>
                 </div>
             </div>
         </section>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,8 +4,38 @@ import "./contact.css";
 const Contact = () => {
     return (
         <section id="contact">
-            <h5>Get In Touch</h5>
-            <h2>Contact Me</h2>
+            <div className="container contact__container">
+                <h5>Get In Touch</h5>
+                <h2>Contact Me</h2>
+                <form action="https://hook.eu1.make.com/7i7wgb5gemo6uvzq1ycn5wc9rropteir">
+                    <input
+                        type="hidden"
+                        name="redirect"
+                        value="https://gregoirevaillant.github.io/portfolio"
+                    />
+                    <input
+                        type="text"
+                        name="fullnamae"
+                        placeholder="Your Full Name"
+                        required
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Your Email Address"
+                        required
+                    />
+                    <textarea
+                        name="message"
+                        placeholder="Your Message"
+                        rows="7"
+                        required
+                    />
+                    <button type="submit" className="btn btn-primary ">
+                        Send Message
+                    </button>
+                </form>
+            </div>
         </section>
     );
 };
