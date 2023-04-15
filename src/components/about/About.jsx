5a9d2528b390e91a7c29817dbdaefbd46945b3b3
Groune <98833627+gregoirevaillant@users.noreply.github.com>
2023-04-15 18:04:32 +0200

    Changes bold name

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -16,21 +16,22 @@ const About = () => {
                 </div>
                 <div className="about__content">
                     <p>
-                        Hello and welcome to my portfolio! My name is Grégoire,
-                        and I am a third year student with a passion for web
-                        development. Although I am currently pursuing a degree
-                        in Business Administration, I have been teaching myself
-                        web development since the end of 2021. I am driven by my
-                        desire to create user-friendly, efficient, and visually
-                        appealing websites that meet the needs of users. With my
-                        dedication to learning and my ability to work both
-                        independently and as part of a team, I am confident that
-                        I have the skills and drive to succeed as a web
-                        developer. Throughout my learning journey, I have honed
-                        my skills in HTML, CSS, Javascript, C++ and I am always
-                        eager to learn more. I have experience working with
-                        HTML, CSS and Javascript, and I am excited to continue
-                        expanding my knowledge in these areas.
+                        Hello and welcome to my portfolio! My name is
+                        <b>Grégoire</b>, and I am a third year student with a
+                        passion for web development. Although I am currently
+                        pursuing a degree in Business Administration, I have
+                        been teaching myself web development since the end of
+                        2021. I am driven by my desire to create user-friendly,
+                        efficient, and visually appealing websites that meet the
+                        needs of users. With my dedication to learning and my
+                        ability to work both independently and as part of a
+                        team, I am confident that I have the skills and drive to
+                        succeed as a web developer. Throughout my learning
+                        journey, I have honed my skills in HTML, CSS,
+                        Javascript, C++ and I am always eager to learn more. I
+                        have experience working with HTML, CSS and Javascript,
+                        and I am excited to continue expanding my knowledge in
+                        these areas.
                         <br />
                         Thank you for taking the time to visit my portfolio, and
                         I look forward to the opportunity to work with you in
